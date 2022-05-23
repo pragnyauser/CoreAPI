2834f1137bce85e4e0a2fb871f51d79432cfdf2a
sandeep.gaonkar@emids.com <sandeep.gaonkar@emids.com>
2022-05-23 22:19:41 +0530

    first commit

---
+++ dotnet-core-xunit/Entities/TestDb/TestDbContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace dotnet_core_xunit.Entities.TestDb
+{
+    public partial class TestDbContext : DbContext
+    {
+
+        public virtual DbSet<Users> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseInMemoryDatabase("SampleMemoryDatabase");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Users>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.CreateDate)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Email).IsRequired();
+
+                entity.Property(e => e.FullName).IsRequired();
+
+                entity.Property(e => e.Password).IsRequired();
+
+                entity.Property(e => e.LastLogin)
+                   .HasColumnType("datetime")
+                   .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Status)
+                    .IsRequired()
+                    .HasDefaultValueSql("((1))");
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
